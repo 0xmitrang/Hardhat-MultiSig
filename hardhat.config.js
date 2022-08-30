78f54b81b55811d420ae72ba741b01801ff2a81d
mitrang <mitrang.lad@smartsensesolutions.com>
2022-08-30 10:56:24 +0530

    configuration and deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+  },
   solidity: "0.8.10",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 };
